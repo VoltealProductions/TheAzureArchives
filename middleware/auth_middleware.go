9808c48482d3f889123a257cb75c034240c88787
Volteal <wendy.a.knight@icloud.com>
2024-08-29 13:51:50 +0200

    Added proper checking of the JWT toekn for authentication to the middleware.

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -2,13 +2,18 @@ package middleware
 
 import (
 	"net/http"
+
+	"github.com/VoltealProductions/TheAzureArcchives/services/auth"
+	"github.com/VoltealProductions/TheAzureArcchives/utils"
 )
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token := r.Header.Get("Authorization")
-		if token != "Bearer token" {
-			http.Error(w, "Unathorized", http.StatusUnauthorized)
+
+		_, err := auth.VerifyToken(token)
+		if err != nil {
+			utils.WriteError(w, http.StatusUnauthorized, err)
 			return
 		}
 
